03ff5a369d930b8a723f158756b86b76c4d8be67
eeehye <58320648+eeehye@users.noreply.github.com>
2019-12-16 16:04:11 +0900

    Add files via upload

---
+++ js/slider.js
@@ -0,0 +1,25 @@
+$(function(){
+    var count = 1; // 이미지 번호
+
+    // 인터벌 함수(함수, 재생시간ms)
+    setInterval(function(){
+              
+      console.log(count);
+      if(count > 2) count = 0;
+      // 모든 이미지 비표시
+      $('.slider img').removeClass('show');
+      // 현재 이미지만 표시
+      $('.slider img')
+        .eq(count)
+        .addClass('show');
+
+      /* 인디케이터(이미지번호표시) */
+      $('.indicator > a').removeClass('show');
+      $('.indicator > a')
+        .eq(count)
+        .addClass('show');
+
+      count++;
+    }, 5000);
+
+  });
